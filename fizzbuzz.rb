5de0b925273845bde8456f5c584934d03a17279a
Tyler Sinks <tyler.sinks@gmail.com>
2020-02-02 19:11:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
-  
+def fizzbuzz(int)
+  if int % 3 && int % 5
+      puts "FizzBuzz"
+    elsif int % 3
+      puts "Fizz"
+    elsif int % 5
+      puts "Buzz"
+    else
+      puts "nil"
+  end
 end
